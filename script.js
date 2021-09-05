5a0c2cb6af8f3273b62e7768f19abd2f863d25ac
PFZM <pfzm@hotmail.com>
2021-09-05 20:45:09 +1000

    added criterias

--- script.js
+++ script.js
@@ -89,43 +89,79 @@ var lowerCasedCharacters = [
 var generateBtn = document.getElementById("generate");
 
 generateBtn.onclick = function () {
-  passwordLenght();
-};
-
-// Selection of the lenght of the passowrds
-//    Criteria: at leat 8 characters no more then 182 charactes
-
-passwordLenght = function () {
-  window.prompt(
-    "How many characters do you want your password to be?\n(Please select between 8 and 182"
-  );
-
-  if (passwordLenght != numericCharacters) {
-    return window.prompt("Please only type numeric characters");
-  } else if (passwordLenght < 8 || passwordLenght > 182) {
-    return window.prompt(
-      "Password must have a lenght of between 8 and 182 characters"
-    );
+  var length = passwordLenght();
+  var lowerCase = lowerCa();
+  var upperCase = upperCa();
+  var numspeCar = numAndSpec();
+  if (length) {
+    console.log(length);
+    console.log(lowerCase);
+    console.log(upperCase);
+    console.log(numspeCar);
   } else {
+    return;
   }
 };
 
-// 3. Selection of characters:
-//    Criteria: inlcude/note include: lowercase, uppercase, numeric, and/or special character
+// Selection of the length of the passowrds
+//    Criteria: at leat 8 characters no more then 182 charactes
+
+passwordLenght = function () {
+  var pLength = window.prompt(
+    "How many characters do you want your password to be?\n(Please select between 8 and 182)"
+  );
+
+  if (isNaN(pLength)) {
+    window.alert("Please only type numeric characters");
+    return false;
+  } else if (pLength < 8 || pLength > 182) {
+    window.alert("Password must have a lenght of between 8 and 182 characters");
+    return false;
+  } else {
+    return parseInt(pLength);
+  }
+};
+
+//  Selection of characters:
+//    Criteria: include lowercase
+lowerCa = function () {
+  var lowCa = confirm(
+    "Do you want your password to have lowercase characters?"
+  );
+  if (lowCa) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//  Selection of characters:
+//    Criteria: include uppercase
+
+upperCa = function () {
+  var uppCa = confirm(
+    "Do you want your password to have uppercase characters?"
+  );
+  if (uppCa) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//  Selection of characters:
+//    Criteria: include numeric and/or special character
+numAndSpec = function () {
+  var numSpec = confirm(
+    "Do you want your password to have numeric and/or special characters?"
+  );
+  if (numSpec) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 //4. Validate: at least one character type should be selected
 //5. Password matches the criteria and is generated
 //6. Passwords shows in HTML
-
-// Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
